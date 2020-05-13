2aa6f9dd4e6917bc1dd6731cd1e3a7fa8389aee0
Annapoorani16 <57026380+Annapoorani16@users.noreply.github.com>
2020-05-13 20:38:47 +0530

    Add files via upload

---
+++ str-no.py
@@ -0,0 +1,17 @@
+#Write a program to give the following output for the given input
+#The number varies from 1 to 99.
+#Eg 1: Input: a1b10
+      # Output: abbbbbbbbbb 
+a=input()
+i=0
+try:
+ while(i<(len(a)-1)):
+    if(a[i+2]>="0" and a[i+2]<="9"): #checking whether double dig exit
+        print(a[i]*int(a[i+1]+a[i+2]),end="")
+        i+=3
+    else:                             #else print the alpha with adjacent dig
+        print(a[i]* int(a[i+1]),end="")
+        i+=2
+except IndexError:                    #if indexerror occurs
+    print(a[i]* int(a[i+1]),end="")
+    pass
